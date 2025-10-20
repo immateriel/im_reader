5ac142c30b8379e1eb1583702e0861c38e0dabff
Elodie Ailleaume <elodie.harkou@gmail.com>
2025-10-20 16:59:27 +0200

    Remove useless puts.

--- app/controllers/im_reader/epub_reader_controller.rb
+++ app/controllers/im_reader/epub_reader_controller.rb
@@ -4,31 +4,25 @@ module ImReader
 
     def show
       @remote_url = params[:url]
-      puts "#### PLOP ####"
       @url = im_reader.remote_epub_reader_url(url: @remote_url)
-      puts "***** OK *****"
     end
 
     def remote
       # Envoyer cette url depuis le BO immateriel
       # @book.book_previews&.last&.download_url
-      puts "***** REMOTE 1 *****"
       url = params[:url]
       uri = URI.parse(url)
       response = fetch_with_redirect(uri)
 
       # on télécharge directement en mémoire
-      puts "***** REMOTE 2 *****"
 
       if response.is_a?(Net::HTTPSuccess)
 
-        puts "***** REMOTE 3.1 *****"
         send_data response.body,
                   filename: File.basename(uri.path.presence || "remote.epub"),
                   type: "application/epub+zip",
                   disposition: "inline"
       else
-        puts "***** REMOTE 3.2 *****"
         render plain: "Impossible de récupérer l’EPUB", status: 404
       end
     end
